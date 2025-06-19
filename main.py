f81a4e053da173f5903658f048037e8921d2a286
7apisLZ <aquaspecks@gmail.com>
2025-06-19 08:41:24 -0400

    Added instructions buttons

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ def main():
     st.title("Invoice Data Parser")
     st.text("Created by Lizzy Zhou")
     st.divider()
+    if st.button("Instructions"):
+        st.write("1) Upload an invoice (must be a scanned pdf)")
+        st.write("2) Download the resulting files:")
+        st.write("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•  invoice.txt - invoice IDs and other info")
+        st.write("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•  accounts.csv - accounts info spreadsheet")
+        st.write("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•  numbers.csv - fax/phone numbers info spreadsheet")
+    else:
+        st.write("")
+
     uploaded_file = st.file_uploader("Upload your invoice here:", type="pdf")
 
     if uploaded_file:
